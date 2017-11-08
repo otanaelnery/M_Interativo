50addc454ed8ffcad0e658ccb2e36b9f281198bf
DESKTOP-TD1CDRO\Natan <otanael.nery@infouniron.com>
2017-11-08 17:10:17 -0300

    pginicial

---
+++ M_Interativo/Controllers/jhjhController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M_Interativo.Controllers
+{
+    public class jhjhController : Controller
+    {
+        // GET: jhjh
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
